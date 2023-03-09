16db873502afc855b0e327cbf730cdc4734ad2b5
Wilson Pinto <wilsonpinto88@gmail.com>
2023-03-09 09:54:51 +0000

    added the Header and Map sections

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from "./Components/Header/Header";
+import List from "./Components/List/List";
+import Map from "./Components/Map/Map";
+
+const App = () => {
+    return (
+        <Fragment>
+            <CssBaseline />
+            <Header />
+            <Grid container spacing={3} style={{ widt: "100%" }}>
+                <Grid item xs={12} md={4}>
+                    <List />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map />
+                </Grid>
+            </Grid>
+        </Fragment>
+    );
 }
 
 export default App;
